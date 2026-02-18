3ca35440ee2dccdd1bf72c1456fa7393d752dff1
CodeWithTM <tukaram.murdekar@gmail.com>
2026-02-18 19:34:24 +0530

    A vs I

---
+++ CApp_Interfaces/AbstractVsInterface.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApp_Interfaces
+{
+    internal class AbstractVsInterface
+    {
+    }
+
+    public abstract class Phone
+    {
+        public string Brand { get; }
+
+        protected Phone(string brand)
+        {
+            Brand = brand;
+        }
+
+        public void Call(string number)
+        {
+            Console.WriteLine($"{Brand} calling {number}");
+        }
+
+        public abstract void PowerOn();
+    }
+
+    public class SmartPhone : Phone
+    {
+        public SmartPhone(string brand) : base(brand) { }
+
+        public override void PowerOn()
+        {
+            Console.WriteLine("Booting OS...");
+        }
+
+        public void BrowseInternet()
+        {
+            Console.WriteLine("Browsing...");
+        }
+    }
+
+
+    // IS-A relationship
+
+    public interface ICamera
+    {
+        void TakePhoto();
+    }
+
+    public interface IGps
+    {
+        void Navigate(string destination);
+    }
+
+    public abstract class PhoneCls
+    {
+        public void Call(string number) { }
+    }
+    public class SmartPhoneCls : PhoneCls, ICamera, IGps
+    {
+        public void TakePhoto() { }
+        public void Navigate(string destination) { }
+    }
+
+    // SmartPhoneCls IS-A PhoneCls
+    // and SmartPhoneCls CAN TakePhoto ,  SmartPhoneCls CAN Navigate
+
+
+
+}
--- CApp_Practice/AbstractInterface.cs
+++ CApp_Practice/AbstractInterface.cs
@@ -6,9 +6,57 @@ using System.Threading.Tasks;
 
 namespace CApp_Practice
 {
+    // Interface - What you can do
+    // Abstract - What you are
+
+
+    // Interface - If you claim you are an IPrinter, you MUST have Print().
+    // Abstract - You are a Vehicle. You share some common logic, but you still have to implement some parts yourself.
+
+    /*
+     Use an interface when:
+
+        ✅ you want a “plug-in point”
+        ✅ you want multiple unrelated classes to share a capability
+        ✅ you want loose coupling
+        ✅ you want dependency injection-friendly design
+    
+    Use an abstract class when:
+        ✅ you want to share common code
+        ✅ you want to enforce a base workflow
+        ✅ you want shared state + helper methods
+        ✅ you want partial implementation
+
+     */
     public class AbstractInterface
     {
     }
+
+    public interface IPaymentService
+    {
+        Task Pay(decimal amt);
+    }
+
+    public class PaypalPaymentService : IPaymentService
+    {
+        public Task Pay(decimal amt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GPayService : IPaymentService
+    {
+        public Task Pay(decimal amt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Because PayPal and Gpay are not “the same type of object” in real life.
+
+    //But they support the same capability: Pay.
+
 
     public interface IAnimal
     {
@@ -29,14 +77,92 @@ namespace CApp_Practice
 
         private int MyProp;        
 
-        public void MakeSound() { } 
+        public void MakeSound() { }         // related / similar functionality
     }
 
     public class Dog : Animal
     {
         public new void MakeSound()
         {
+            
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public new void MakeSound()
+        {
 
         }
     }
+
+
+    /*
+     File processing pipeline:
+
+    Imagine you want a standard workflow:
+
+        1. Validate
+
+        2. Read file
+
+        3. Process file data
+
+        4. Save output
+     */
+
+    public abstract class FileProcessor
+    {
+        public void Run(string path)
+        {
+            Validate(path);
+            var data = Read(path);
+            Process(data);
+            Save(data);
+        }
+
+        protected virtual void Validate(string path) { }    // optional, you may or may not validate a file
+        
+        // but you have to follow all the below steps
+        protected abstract string Read(string path);
+        protected abstract void Process(string data);
+        protected abstract void Save(string data);
+    }
+
+    public class CSVFileProcessor : FileProcessor
+    {
+        protected override void Process(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Save(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class XMLFileProcessor : FileProcessor
+    {
+        protected override void Process(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Save(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
